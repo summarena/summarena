5f3b9eb73ec603f559792e12771829d069fa6012
Warren He <w@oasislabs.com>
2025-08-20 16:56:38 -0700

    state: add sqlite ingest impl

--- interfaces/src/state.rs
+++ interfaces/src/state.rs
@@ -1,6 +1,26 @@
+use rusqlite::Connection;
+
 use crate::defs::InputItem;
 use crate::defs::LiveSourceSpec;
 
+fn get_db_connection() -> Connection {
+    Connection::open("omni.db").unwrap()
+}
+
 pub fn ingest(source: &LiveSourceSpec, input_item: InputItem) {
-    // todo: save item
+    let conn = get_db_connection();
+    conn.execute(
+        "
+            INSERT INTO input_items
+                (uri, live_source_uri, text, vision)
+            VALUES
+                (?1, ?2, ?3, ?4)
+        ",
+        (
+            &input_item.uri,
+            &input_item.live_source_uri,
+            &input_item.text,
+            &input_item.vision.as_deref(),
+        ),
+    ).unwrap();
 }
