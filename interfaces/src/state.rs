3bdd3bd7be814aff0c5f3151cf85965222cd33b1
Warren He <w@oasislabs.com>
2025-08-21 16:46:13 -0700

    state: switch to sqlx

--- interfaces/src/state.rs
+++ interfaces/src/state.rs
@@ -1,25 +1,47 @@
-use rusqlite::Connection;
+use sqlx::Connection;
+use sqlx::SqliteConnection;
+use sqlx::migrate::MigrateDatabase;
 
 use crate::defs::InputItem;
+use crate::defs::LiveSourceSpec;
 
-fn get_db_connection() -> Connection {
-    Connection::open("omni.db").unwrap()
+async fn get_db_connection() -> SqliteConnection {
+    SqliteConnection::connect("sqlite:omni.db").await.unwrap()
 }
 
-pub async fn ingest(input_item: InputItem) {
-    let conn = get_db_connection();
-    conn.execute(
-        "
-            INSERT INTO input_items
-                (uri, live_source_uri, text, vision)
-            VALUES
-                (?1, ?2, ?3, ?4)
-        ",
-        (
-            &input_item.uri,
-            &input_item.live_source_uri,
-            &input_item.text,
-            &input_item.vision.as_deref(),
-        ),
-    ).unwrap();
+pub async fn migrate() {
+    sqlx::Sqlite::create_database("sqlite:omni.db").await.unwrap();
+    let mut conn = get_db_connection().await;
+    sqlx::migrate!("./migrations").run(&mut conn).await.unwrap();
+}
+
+pub async fn create_live_source_spec(live_source_spec: &LiveSourceSpec) {
+    let mut conn = get_db_connection().await;
+    sqlx::query("
+        INSERT OR IGNORE INTO live_source_specs
+            (uri)
+        VALUES
+            (?1)
+    ")
+    .bind(&live_source_spec.uri)
+    .execute(&mut conn)
+    .await
+    .unwrap();
+}
+
+pub async fn ingest(input_item: &InputItem) {
+    let mut conn = get_db_connection().await;
+    sqlx::query("
+        INSERT OR IGNORE INTO input_items
+            (uri, live_source_uri, text, vision)
+        VALUES
+            (?1, ?2, ?3, ?4)
+    ")
+    .bind(&input_item.uri)
+    .bind(&input_item.live_source_uri)
+    .bind(&input_item.text)
+    .bind(&input_item.vision)
+    .execute(&mut conn)
+    .await
+    .unwrap();
 }
